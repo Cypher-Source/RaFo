cda3061dc110ae10f8082cc92b5e4664e2cd680a
Logidasakthi <banulogida2000@gmail.com>
2020-07-23 20:56:59 +0530

    [IMP] postPage : postpage completed
    All the required components are placed and checked for responsiveness. Naming conventions and commenting are perfectly done.Photopage is completely over and post page has a small alignment to be done.
    Balance : Post page select alert heading center alignment is not done.

--- src/app/pages/post/post.page.ts
+++ src/app/pages/post/post.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.page.html',
   styleUrls: ['./post.page.scss'],
 })
 export class PostPage implements OnInit {
-
+  customAlertOptions: any = {
+    cssClass: 'my-alert',
+    header: 'Category',
+    // translucent: true
+  };
   constructor() { }
-
   ngOnInit() {
   }
-   }
+}
