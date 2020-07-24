975ac5cce79f1e054381b9d4cbd0b0f3fd65faa6
Musaraf2000 <mohammedmusaraf.psnacet.edu.in>
2020-07-25 01:27:06 +0530

    [IMP] Login page, Signup page: Validators added

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  get username() {
+    return this.signupForm.get('username');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get cpassword() {
+    return this.signupForm.get('cpassword');
+  }
+
+  public errorMessages = {
+    
+    username: [
+      { type: 'required', message: 'Username is required'},
+      { type: 'maxlength',
+      message: 'Username cant be longer than 20 characters'}
+      
+    ],
+    email: [
+      { type: 'required', message: 'Email is required'},
+      { type: 'pattern', message: 'Please enter a valid email address'}
+    ],
+    password: [
+      { type: 'required', message: 'Password is required'},
+      { type: 'maxlength',
+      message: 'password cant be longer than 20 characters'},
+      { type: 'minlength',
+      message: 'State cant be less than 5 characters'},
+      { type: 'pattern',
+      message: 'must have a special character'}
+      
+    ],
+    cpassword: [
+      { type: 'required', message: 'Password is required'},
+      { type: 'pattern', message: 'Password mismatch'}
+    ]
+
+  }
+
+  signupForm = this.formBuilder.group ({
+  username: ['', Validators.required, Validators.maxLength(20)],
+  email: ['', [Validators.required, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]],
+  password: ['', Validators.required, Validators.maxLength(20), Validators.minLength(5), Validators.pattern(/[^((0-9)|(a-z)|(A-Z)|\s)]/)],
+  cpassword: ['', Validators.required]
+}); 
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
   }
 
+  public submit() {
+    console.log(this.signupForm.value);
+    
+  }
+
+  signup(){
+    this.router.navigateByUrl('/signup');
+  }
 }
