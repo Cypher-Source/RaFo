1b1c92c6a6a3951892d4098ac042792987c9f684
navayuvan-sb <ns.navayuvan@gmail.com>
2020-07-24 15:06:28 +0530

    [IMP] HOME: Plus Fab wired to open Post Modal

--- src/app/pages/post/post.page.ts
+++ src/app/pages/post/post.page.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.page.html',
-  styleUrls: ['./post.page.scss'],
+  selector: "app-post",
+  templateUrl: "./post.page.html",
+  styleUrls: ["./post.page.scss"],
 })
 export class PostPage implements OnInit {
   customAlertOptions: any = {
-    cssClass: 'my-alert',
-    header: 'Category',
+    cssClass: "my-alert",
+    header: "Category",
     // translucent: true
   };
-  constructor() { }
-  ngOnInit() {
+  constructor(private modalController: ModalController) {}
+  ngOnInit() {}
+
+  // dismiss the modal view
+  closeModal() {
+    this.modalController.dismiss();
   }
 }
--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { PostPage } from "../post/post.page";
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  selector: "app-tabs",
+  templateUrl: "tabs.page.html",
+  styleUrls: ["tabs.page.scss"],
 })
 export class TabsPage {
+  constructor(private modalController: ModalController) {}
 
-  constructor() {}
-
+  // open post modal
+  async openPostModal() {
+    const modal = await this.modalController.create({
+      component: PostPage,
+    });
+    return await modal.present();
+  }
 }
