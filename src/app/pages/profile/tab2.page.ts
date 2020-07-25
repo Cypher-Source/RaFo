598dd253f025efab39109b8fd6ab44e55d88a822
navayuvan-sb <ns.navayuvan@gmail.com>
2020-07-25 12:08:16 +0530

    [IMP] PROFILE: Profile name edit option implemented

--- src/app/pages/profile/tab2.page.ts
+++ src/app/pages/profile/tab2.page.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
-
 export class Tab2Page {
+  profileNameEditStatus: boolean = false;
 
-  constructor(
-  ) {}  
+  profileName: string = "Navayuvan";
 
+  constructor() {}
 
+  // profile name edit
+  editActionPerformed(): void {
+    this.profileNameEditStatus = !this.profileNameEditStatus;
+  }
 }
