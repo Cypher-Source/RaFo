c0f762aa29fb2567a3d8b971587a9c11bba579ac
navayuvan-sb <ns.navayuvan@gmail.com>
2020-07-25 12:36:24 +0530

    [IMP] SERVICE: Response Views - Loading screen and Toast message added

--- src/app/services/ResponseViews/response-view.service.ts
+++ src/app/services/ResponseViews/response-view.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { ToastController, LoadingController } from "@ionic/angular";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ResponseViewService {
+  constructor(
+    private toastController: ToastController,
+    private loadingController: LoadingController
+  ) {}
 
-  constructor() { }
+  // show toast message
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+    });
+    toast.present();
+  }
+
+  // show loading screen
+  async getLoadingScreen() {
+    const loading = await this.loadingController.create({
+      cssClass: "loading-spinner",
+      spinner: "lines",
+      message: null,
+      translucent: true,
+    });
+    return loading;
+  }
 }
